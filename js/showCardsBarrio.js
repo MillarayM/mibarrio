faa9c27e4985ed6d5453a95a7c5681de8ef1c809
ElpolMorris <pmorales.contacto@gmail.com>
2021-01-16 22:15:30 -0300

    cards modales barrios + proyectos funcionamiento basico

--- js/showCardsBarrio.js
+++ js/showCardsBarrio.js
@@ -1,5 +1,3 @@
-//hasta aqui funciona
-
 const db = firebase.firestore();
 
 //div contenedores
@@ -14,17 +12,40 @@ const onGetTasks = (callback) => db.collection("barrios").onSnapshot(callback);
 
 //plantilla barrios
 
-const plantillaBarrios = (barrio,doc) =>
-	(`<div class="card card-body mt-2 px-1 col-4 border-primary">
+const plantillaBarrios = (
+	barrio
+) => `<div class="card card-body mt-2 px-1 col-4 border-primary">
         <img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ03luuGw4KJi-L-9NoFeC3BWgS5LGnT4E6UA&usqp=CAU>
         <h3 class="h5">${barrio.nombre}</h3>
         <p>${barrio.comuna}</p>
         <p>${barrio.region}</p>
         <div class="text-center">
-            <button class="btn btn-success btn-get-barrios" data-id=${doc.id} data-bs-toggle="modal" data-bs-target="#exampleModal" onclick=mostrarBarrios(1,'${barrio.nombre}')>Ver Barrio</button>
-            <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal2" onclick=mostrarBarrios(2)>Ver Proyectos</button>
+            <button class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#exampleModal" onclick= "mostrarBarrios('${barrio.nombre}',1)">Ver Barrio</button>
+            <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal2" onclick="mostrarBarrios('${barrio.nombre}',2)">Ver Proyectos</button>
         </div>
-    </div>`)
+    </div>`;
+const plantillaProyectos = ()=>{
+    return `<div class="modal-header">
+    <h5 class="modal-title" id="exampleModalLabel">Listado Proyectos</h5>
+</div>
+<div class="modal-body">
+    <table class="table" >
+    <thead>
+    <tr>
+        <th scope="col">Nombre proyecto</th>
+        <th scope="col">Descripcion</th>
+        <th scope="col">Fecha inicio</th>
+        <th scope="col">Fecha Fin</th>
+    </tr>
+    </thead>
+    <tbody id= "tablaProyectos">                  
+    </tbody>
+</table>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick= deleteBarrio("infoBarrios")>Cerrar</button>
+</div>`
+}
 
 //carga de cards de barrios
 window.addEventListener("DOMContentLoaded", (e) => {
@@ -32,7 +53,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
 		containerBarrios.innerHTML = "";
 		querySnapshot.forEach((doc) => {
 			const barrio = doc.data();
-			containerBarrios.innerHTML += plantillaBarrios(barrio,doc);
+			containerBarrios.innerHTML += plantillaBarrios(barrio, doc);
 			listaOpcionesBarrios.insertAdjacentHTML(
 				"beforeend",
 				`<option value='${barrio.nombre}'>${barrio.nombre}</option>`
@@ -68,49 +89,73 @@ filtradorBarrios.addEventListener("input", async (e) => {
 });
 
 //const seccionModal = document.getElementById("section-modales");
-const divInfoBarrios = document.getElementById("infoBarrios")
-const divInfoProyectos = document.getElementById('infoProyectos')
+const divInfoBarrios = document.getElementById("infoBarrios");
+const divInfoProyectos = document.getElementById("infoProyectos");
 
-const mostrarBarrios = (idModal,idDoc)=>{
-    let consultaBarrios = db.collection('barrios')
-    const a = idDoc
-    console.log(a)
-    divInfoProyectos.innerHTML = ''
-    divInfoBarrios.innerHTML = ''
-    idModal == 1 ? 
-    divInfoBarrios.insertAdjacentHTML('beforeend',
-    `<div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Barrios</h5>
-    </div>
-    <div class="modal-body">
-        ...
-    </div>
-    <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick= deleteBarrio("infoBarrios")>Cerrar</button>
-    </div>`) 
-: 
-    divInfoProyectos.insertAdjacentHTML('beforeend',
-    `<div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Proyectos</h5>
-    </div>
-    <div class="modal-body">
-    ...
-    </div>
-    <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick= deleteBarrio("infoProyectos")>Cerrar</button>
-    </div>`)
-}
+const mostrarBarrios = async (idDoc, id) => {    
+	try {
+		if (id == 1) {
+			await db
+				.collection("barrios")
+				.where("nombre", "==", idDoc)
+				.get()
+				.then((querySnapshot) => {
+					querySnapshot.forEach(function (doc) {
+                        const resultado = doc.data();
+						divInfoBarrios.innerHTML = "";
+						divInfoBarrios.insertAdjacentHTML(
+							"beforeend",
+							`<div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">${resultado.nombre}</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div>Lugar: ${resultado.nombre}</div>
+                        <div>Ubicacion: ${resultado.ubicacion}</div>
+                        <div>Comuna: ${resultado.comuna}</div>
+                        <div>Region: ${resultado.region}</div>
+                        <div>Historial: ${resultado.historial}</div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick= deleteBarrio("infoBarrios")>Cerrar</button>
+                    </div>`
+						);
+					});
+				});
+		}
+		if (id == 2) {
+            const plantilla = plantillaProyectos()
+            divInfoProyectos.innerHTML = "";
+            divInfoProyectos.insertAdjacentHTML("beforeend",plantilla)
+            const tabla = document.getElementById("tablaProyectos")
+			await db
+				.collection("proyectos")
+				.where("barrio", "==", idDoc)
+				.get()
+				.then((querySnapshot) => {
+					querySnapshot.forEach(function (doc) {
+                        const res = doc.data();
+                        console.log(res)
+						//divInfoProyectos.innerHTML = "";
+						tabla.insertAdjacentHTML(
+							"beforeend",
+							`<tr>
+                                <td>${res.nombre}</td>
+                                <td>${res.descripcion}</td>
+                                <td>${res.fechaInicio}</td>
+                                <td>${res.fechaFin}</td>                        
+                            </tr>`
+						);
+					});
+                })
+		}
+	} catch (error) {
+		console.log("hubo un error", error);
+	}
+};
 
-const deleteBarrio = (divs)=>{
-    let a = document.getElementById(divs)
-    while (a.firstChild) {
-        a.removeChild(a.firstChild);
-    }
-}
-
-// const botonesBarrioModal = document.querySelectorAll('.btn-get-barrios')
-// botonesBarrioModal.forEach((btn)=>{
-//     btn.addEventListener("click",async (e)=>{
-//         console.log(e.target.dataset.id);
-//     })
-// })
+const deleteBarrio = (divs) => {
+	let a = document.getElementById(divs);
+	while (a.firstChild) {
+		a.removeChild(a.firstChild);
+	}
+};
